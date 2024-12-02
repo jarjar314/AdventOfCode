e2a7f18e9e3d1d8ef98e5a5d0adc249945959d78
Jarjar <jaylesup_interne@yahoo.fr>
2024-12-02 09:12:44 +0100

    Day 1 and 2 of 2024

---
+++ AOC2024/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace AOC2024
+{
+    internal class Day1
+    {
+        static void Main()
+        {
+            string className = MethodBase.GetCurrentMethod()!.DeclaringType!.Name;
+            using (var sr= new StreamReader($"Resources/{className}.txt"))
+            {
+                var l1 = new List<int>();
+                var l2 = new List<int>();
+                while (!sr.EndOfStream)
+                {
+                    var pair = sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    l1.Add(int.Parse(pair[0]));
+                    l2.Add(int.Parse(pair[1]));
+                }
+                l1.Sort();
+                l2.Sort();
+                long res = 0;
+                for (int i = 0; i < l1.Count; i++)
+                {
+                    res += Math.Abs(l1[i] - l2[i]);
+                }
+                Console.WriteLine(res);
+                var group = l2.GroupBy(c => c).ToDictionary(gr => gr.Key, gr => gr.Count());
+                long sum = 0;
+                for(int i = 0; i < l1.Count; i++)
+                {
+                    group.TryGetValue(l1[i], out var c);
+                    sum += l1[i] * c;
+                }
+                Console.WriteLine(sum);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ AOC2024/Day2.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Reflection;
+
+namespace AOC2024
+{
+    internal class Day2
+    {
+        static void Main()
+        {
+            string className = MethodBase.GetCurrentMethod()!.DeclaringType!.Name;
+            using (var sr = new StreamReader($"Resources/{className}.txt"))
+            {
+                var l1 = new List<List<int>>();
+                while (!sr.EndOfStream)
+                {
+                    l1.Add(sr.ReadLine().Split().Select(int.Parse).ToList());
+                }
+                Console.WriteLine(l1.Count(c => increasing(c) || decreasing(c)));
+                Console.WriteLine(l1.Count(c => increasing(c) || decreasing(c) || dampen(c)));
+
+            }
+
+            bool increasing(List<int> l)
+            {
+                int last = int.MinValue;
+                foreach (var n in l)
+                {
+                    if (n <= last) return false;
+                    if (last != int.MinValue && n - last > 3) return false;
+                    last = n;
+                }
+                return true;
+            }
+            bool decreasing(List<int> l)
+            {
+                int last = int.MaxValue;
+                foreach (var n in l)
+                {
+                    if (n >= last) return false;
+                    if (last != int.MaxValue && last - n > 3) return false;
+                    last = n;
+                }
+                return true;
+            }
+
+            bool dampen(List<int> l)
+            {
+                for (int i = 0; i < l.Count; i++)
+                {
+                    var l2 = new List<int>();
+                    for (int j = 0; j < l.Count; j++)
+                    {
+                        if (j == i) continue;
+                        l2.Add(l[j]);
+                    }
+                    if (increasing(l2)) return true;
+                    if (decreasing(l2)) return true;
+                }
+                return false;
+            }
+        }
+    }
+}
